d2c7a0e80bba022fd2f046551fa64cef1e8f933e
Anurag <anuragtivari850@gmail.com>
2024-08-31 00:05:31 +0530

    sidebar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,10 @@
 import { cn } from "@/lib/utils";
 import { UserButton } from "@clerk/nextjs";
-import { Menu } from "lucide-react";
+import { Menu, Sparkles } from "lucide-react";
 import { Poppins } from "next/font/google";
 import Link from "next/link";
+import { Button } from "./ui/button";
+import { ModeToggle } from "./modeToggle";
 
 const font = Poppins({
   weight: "600",
@@ -11,7 +13,7 @@ const font = Poppins({
 
 export const Navbar = () => {
   return (
-    <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary">
+    <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary h-16">
       <div className="flex items-center">
         <Menu className="block md:hidden" />
         <Link href="/">
@@ -25,7 +27,12 @@ export const Navbar = () => {
           </h1>
         </Link>
       </div>
-      <div className="flex items-center gap-3">
+      <div className="flex items-center gap-x-3">
+        <Button variant="premium" size="sm">
+          Upgrade
+          <Sparkles className="h-4 w-4 fill-white text-white ml-2" />
+        </Button>
+        <ModeToggle />
         <UserButton />
       </div>
     </div>
